c1205b348c2f58d2a759720add77c3adb3f14db3
Robert Campbell <robert.g.campbell@ibm.com>
2022-01-31 22:11:46 -0500

    Fix custom metric for winner

--- src/custom_metrics.py
+++ src/custom_metrics.py
@@ -3,7 +3,7 @@ Custom metric to record winner for win percentage
 """
 
 from typing import Dict
-import argparse
+import math
 import numpy as np
 
 import ray
@@ -16,6 +16,26 @@ from ray.rllib.agents.callbacks import DefaultCallbacks
 
 
 class CustomMetricsCallback(DefaultCallbacks):
+    def __init__(self):
+        super().__init__()
+        self.actions = {"attacker": [], "defender": []}
+
+    def check_winner(self, episode):
+        attacker_reward = episode.last_reward_for("attacker")
+        defender_reward = episode.last_reward_for("defender")
+
+        try:
+            assert math.floor(abs(attacker_reward)) == math.floor(abs(defender_reward))
+        except AssertionError:
+            raise AssertionError(f"Attacker reward: {attacker_reward} - Defender reward - {defender_reward}")
+
+        if attacker_reward > 0:
+            return 1
+        elif defender_reward > 0:
+            return -1
+        elif attacker_reward == 0 and defender_reward == 0:
+            return 0
+  
     def on_episode_start(self, worker: RolloutWorker, base_env: BaseEnv,
                          policies: Dict[str, Policy],
                          episode: MultiAgentEpisode, **kwargs):
@@ -32,17 +52,10 @@ class CustomMetricsCallback(DefaultCallbacks):
     def on_episode_end(self, worker: RolloutWorker, base_env: BaseEnv,
                        policies: Dict[str, Policy], episode: MultiAgentEpisode,
                        **kwargs):
-
-        winner = None
-        for key, value in episode.agent_rewards.items():
-            print(episode.agent_rewards)
-            if value == 1:
-                winner = 0 if key[0] == "attacker" else 1
-        if winner == None:
-            winner = -1
+        
+        winner = self.check_winner(episode)
 
         print("episode {} ended with winner {}".format(episode.episode_id, winner))
-        episode_end_mapping = {"tie": -1, "attacker": 0, "defender": 1}
         episode.custom_metrics["episode_winner"] = winner
         episode.hist_data["episode_winners"].append(winner)
 
@@ -52,7 +65,17 @@ class CustomMetricsCallback(DefaultCallbacks):
             agent_id: str, policy_id: str, policies: Dict[str, Policy],
             postprocessed_batch: SampleBatch,
             original_batches: Dict[str, SampleBatch], **kwargs):
-        print("postprocessed {} steps".format(postprocessed_batch.count))
+        #print("postprocessed {} steps".format(postprocessed_batch.count))
         if "num_batches" not in episode.custom_metrics:
             episode.custom_metrics["num_batches"] = 0
-        episode.custom_metrics["num_batches"] += 1
+        episode.custom_metrics["num_batches"] += 1 
+
+        #if agent_id == 'attacker':
+        print('agent_id = {}'.format(agent_id))
+        print('episode = {}'.format(episode.episode_id))
+
+        #print("on_postprocess_traj info = {}".format(info))
+        #print("on_postprocess_traj sample_obj = {}".format(sample_obj))
+        #print('actions = {}'.format(postprocessed_batch.columns(["actions"])))
+
+        self.actions[agent_id].append(postprocessed_batch.columns(["actions"]))
